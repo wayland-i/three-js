cdf1aa059d92c110e6c86636a43b5c60a07b55e3
Ian Iversen <ianiversen1@gmail.com>
2024-01-28 21:16:14 -0500

    docs: cleaned old fbx attempt

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 
 //renders our space where three will work
 const renderer = new THREE.WebGLRenderer();
@@ -7,7 +6,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
-
 
 
 // creating the scene
@@ -31,27 +29,6 @@ const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
-const fbxLoader = new FBXLoader();
-fbxLoader.load('./ChrisTpos.fbx')
-
-// const image = document.getElementById('image')
-
-// image.src = './yes.jpeg'
-
-
-
-// const fbxLoader = new THREE.FBXLoader();
-
-// Load the FBX file
-// fbxLoader.load('../src/ChrisTpose.fbx', (fbx) => {
-//     // Adjust the scale, position, or rotation if needed
-//     fbx.scale.set(0.1, 0.1, 0.1);
-//     fbx.position.set(0, 0, 0);
-//     fbx.rotation.set(0, Math.PI / 2, 0);
-  
-//     // Add the FBX object to the scene
-//     scene.add(fbx);
-// });
 
 
 renderer.render(scene, camera);
